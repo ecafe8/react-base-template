8eb968ebdf57b12368bd09f2e2cd1f43d08bdac4
zhang <zhangbin@sinxia.com>
2017-08-17 19:08:18 +0800

    update

---
+++ webpack.config.update.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = (config) => {
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      components: path.resolve(process.cwd(), './src') + '/components',
+    }
+  }
+};
