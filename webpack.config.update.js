e722203bcb0f0a0527793f271a4bb6ee63197394
zhang <zhangbin@sinxia.com>
2017-08-17 19:17:45 +0800

    fix

--- webpack.config.update.js
+++ webpack.config.update.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = (config) => {
-  config.resolve = {
+  return config.resolve = {
     ...config.resolve,
     alias: {
       components: path.resolve(process.cwd(), './src') + '/components',
